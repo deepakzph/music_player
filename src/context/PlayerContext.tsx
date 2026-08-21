636b597cfc80464add1f7a09aba6d93b8fd8cfbe
Deepak-Rana <deepak.rana@zakipoint.com>
2026-08-21 06:24:10 +0545

    create playere context

--- src/context/PlayerContext.tsx
+++ src/context/PlayerContext.tsx
@@ -16,3 +16,18 @@ interface PlayerState {
   searchQuery: string;
   isMiniPlayer: boolean;
 }
+
+type Action =
+  | { type: "PLAY_TRACK"; libraryIndex: number }
+  | { type: "TOGGLE_PLAY" }
+  | { type: "SET_PLAYING"; isPlaying: boolean }
+  | { type: "NEXT" }
+  | { type: "PREVIOUS" }
+  | { type: "SET_PROGRESS"; progress: number; duration: number }
+  | { type: "SET_VOLUME"; volume: number }
+  | { type: "TOGGLE_MUTE" }
+  | { type: "TOGGLE_SHUFFLE" }
+  | { type: "CYCLE_REPEAT" }
+  | { type: "TOGGLE_FAVORITE"; id: string }
+  | { type: "SET_SEARCH"; query: string }
+  | { type: "TOGGLE_MINI_PLAYER" };
